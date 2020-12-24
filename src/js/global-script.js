2fec5d999a346b85ce2d7b63d9f6fd41578796de
amstelart <amsteldroid@gmail.com>
2020-12-24 19:31:30 +0300

    все блоки

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
   $(".reviews__carousel").owlCarousel({
     items: 1,
     nav: true,
+    dots: false,
     loop: true,
     center: true,
     navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;stroke: #fff;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>','<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;stroke: #fff;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>']
