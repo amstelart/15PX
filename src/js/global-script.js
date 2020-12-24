18fa83fde7d2e0f0445c2848923228134c96a882
amstelart <amsteldroid@gmail.com>
2020-12-24 21:56:23 +0300

    +

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -9,6 +9,31 @@ $(document).ready(function () {
     center: true,
     navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;stroke: #fff;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>','<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;stroke: #fff;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>']
   });
+  $(".partners__carousel").owlCarousel({
+    items: 4,
+    nav: false,
+    margin: 18,
+    loop: true,
+    center: false,
+    navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;stroke: #fff;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>','<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;stroke: #fff;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
+    responsive : {
+      0 : {
+        items: 2,
+        nav: false,
+        loop: true,
+        center: false,
+      },
+      480 : {
+        items: 2,
+      },
+      768 : {
+        items: 3,
+      },
+      992 : {
+        items: 4,
+      }
+    }
+  });
   $(".certificate__carousel").owlCarousel({
     items: 4,
     nav: true,
@@ -33,6 +58,20 @@ $(document).ready(function () {
         items: 4,
       }
     }
+  });
+
+  $("#main-nav").on("click", "a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
+
+    //забираем идентификатор бока с атрибута href
+    var id = $(this).attr('href'),
+
+      //узнаем высоту от начала страницы до блока на который ссылается якорь
+      top = $(id).offset().top;
+
+    //анимируем переход на расстояние - top за 1500 мс
+    $('body,html').animate({ scrollTop: top }, 1500);
   });
 });
 
